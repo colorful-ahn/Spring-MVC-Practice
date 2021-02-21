cf79d9038b07d43b3eb273cd01c36a03cbffd00e
ahn kyung rok <akrnote97@gmail.com>
2021-02-21 22:04:42 +0900

    use dispatcher

--- webprj/src/main/java/webprj/IndexController.java
+++ webprj/src/main/java/webprj/IndexController.java
@@ -1,5 +1,19 @@
 package webprj;
 
-public class IndexController {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class IndexController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("data","ho ang");
+		mv.setViewName("/WEB-INF/view/index.jsp");
+		return mv;
+	}
 
 }
--- webprj/src/main/webapp/WEB-INF/view/index.jsp
+++ webprj/src/main/webapp/WEB-INF/view/index.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<h1>Hello world!</h1>
+	<h1>Hello world! ${data}</h1>
 </body>
 </html>
